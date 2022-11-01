7f6ca9f33c3ec5910afb864d46bc26229ebe12d8
dgueudre <dgueudre@gmail.com>
2022-11-01 11:02:40 +0100

    config vscode

---
+++ .vscode/.php-cs-fixer.php
@@ -0,0 +1,14 @@
+<?php
+
+// https://cs.symfony.com/doc/rules/
+$config = new PhpCsFixer\Config();
+$config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        'no_alternative_syntax' => ['fix_non_monolithic_code' => false],
+        'echo_tag_syntax' => ['format' => 'short'],
+    ])
+;
+
+return $config;
